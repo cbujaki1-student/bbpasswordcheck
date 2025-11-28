595e50be1e66e12d5f47101abc4bcff6e0c9dc7a
cbujaki1-student <cbujaki1@emich.edu>
2025-11-28 12:15:14 -0500

    Update service name in bblauncher.sh to bbpasswordsv10

--- bblauncher.sh
+++ bblauncher.sh
@@ -3,7 +3,8 @@
 # Launching service which calls password reset script once
 if [ ! -f /home/bbuser/.firstboot_done ]; then
     systemctl --user daemon-reload
-    systemctl --user enable bbpasswordsv9.service
-    systemctl --user start bbpasswordsv9.service
+    systemctl --user enable bbpasswordsv10.service
+    systemctl --user start bbpasswordsv10.service
     touch /home/bbuser/.firstboot_done
 fi
+
